328c9a12304c3dad68026b01d1a742cfc79abe4a
guc1 <164617227+guc1@users.noreply.github.com>
2025-06-08 11:44:58 +0200

    Improve model selection UX

--- components/model-selector.tsx
+++ components/model-selector.tsx
@@ -61,7 +61,7 @@ export function ModelSelector({
           variant="outline"
           className="md:px-2 md:h-[34px]"
         >
-          {selectedChatModel?.name}
+          <span className="mr-1">{selectedChatModel?.name}</span>
           <ChevronDownIcon />
         </Button>
       </DropdownMenuTrigger>
